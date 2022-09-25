edf10f83eb856ce379aa1680fd06d248c146fdfe
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-09-24 22:50:01 -0300

    feat: add clap (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::*;
 fn main() {
-    println!("Hello, world!");
+    let _ = Command::new("jr - jq rusty brother")
+        .author("\ngithub.com/ddanielsantos")
+        .version("- v0.0.5")
+        .about("\nLow scale JSON processor")
+        // .arg(Arg::new("in_file"))
+        .after_help("Just like jq and jp, but now in Rust ")
+        .get_matches();
 }
